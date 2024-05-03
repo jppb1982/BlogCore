99f83af2a238784f32427f106be8c4551c3bb320
Juan Pablo Ponce <jppb1982@gmail.com>
2024-05-03 11:19:35 -0300

    Seccion 2 del curso terminada

---
+++ BlogCore.Modelos/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un nombre para la categoría")]
+        [Display( Name = "Nombre de categoría")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Oreden de visualización")]
+        public int? Orden { get; set; }
+    }
+}
